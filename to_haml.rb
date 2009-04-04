da1ade0fea7bba94b0946680d0725a9da6fc6b4c
Adam Bachman <adam@slsdev.net>
2009-04-04 15:08:07 -0400

    added erb to haml converter

---
+++ to_haml.rb
@@ -0,0 +1,18 @@
+# Convert all .erb files in the project to .haml
+
+class ToHaml
+  def initialize(path)
+    @path = path
+  end
+  
+  def convert!
+    Dir["#{@path}/**/*.erb"].each do |file|
+      puts "converting #{file}"
+      `html2haml -rx #{file} #{file.gsub(/\.erb$/, '.haml')}`
+    end
+  end
+end
+
+path = File.join(File.dirname(__FILE__), 'app', 'views')
+ToHaml.new(path).convert!
+
