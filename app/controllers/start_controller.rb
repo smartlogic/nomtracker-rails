40f1261d4a377707e47ed5b3816dc28b8181fbed
John Trupiano <jtrupiano@gmail.com>
2009-05-02 11:02:09 -0400

    Add some basic specs to start_controller_test.

--- app/controllers/start_controller.rb
+++ app/controllers/start_controller.rb
@@ -3,10 +3,13 @@ class StartController < ApplicationController
 
 
   def index
-    if logged_in?
-      logger.debug('logged in')
+    if !logged_in?
+      render :action => 'splash'
+      return
     end
 
+    logger.debug('logged in')
+    #@transactions = current_user.tra
   end
 
 end
--- test/functional/start_controller_test.rb
+++ test/functional/start_controller_test.rb
@@ -1,8 +1,38 @@
-require 'test_helper'
+require File.dirname(__FILE__) + '/../test_helper'
 
 class StartControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  
+  context "An unauthenticated user accesses the root of the site" do
+    setup do
+      log_out
+      get(:index)
+    end
+    
+    should "render the splash page" do
+      assert_response :success
+      assert_template 'splash'
+    end
+    
+    should "display a login link in the header" do
+      
+    end
+    
   end
+  
+  context "An authenticated user accesses the root of the site" do
+    setup do
+      log_in adam
+      get(:index)
+    end
+    
+    should "render the transaction log" do
+      assert_response :success
+      assert_template 'index'
+    end
+    
+    should "display a logout link in the header" do
+      #assert_select "#header #actions"
+    end
+  end
+  
 end
