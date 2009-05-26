82b7bb94d4d30ee3881a6db45b44955108e49346
John Trupiano <jtrupiano@gmail.com>
2009-05-25 21:25:51 -0400

    Display recent transactions on dashboard.

--- app/helpers/transactions_helper.rb
+++ app/helpers/transactions_helper.rb
@@ -1,2 +1,18 @@
 module TransactionsHelper
+  
+  def to_sentence(txn)
+    if txn.amount > 0
+      "<em>You</em> lent #{h txn.other_person.email}"
+    else
+      "#{h txn.other_person.email} lent <em>you</em>"
+    end
+  end
+  
+  def full_description(txn)
+    ret = ""
+    ret << " on #{txn.when}" unless txn.when.blank?
+    ret << " for #{txn.description}" unless txn.description.blank?
+    ret
+  end
+  
 end
