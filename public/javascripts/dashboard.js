8772c333594cc736fe60d5528c332e8298c7b563
John Trupiano <jtrupiano@gmail.com>
2009-06-12 15:49:50 -0400

    Update links in about.htm.  Apply some jslint suggestions to dashboard.js

--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -1,31 +1,23 @@
+function updateGlobals(obj) {
+  // Nomworth
+  if (obj.nomworth !== undefined) {
+    var nomworth = $('nomworth');
+    if (obj.nomworth >= 0) {
+      nomworth.className = "credit";
+    } else {
+      nomworth.className = "debt";
+      obj.nomworth *= -1;
+    }
+    $('nomworth').update("$" + obj.nomworth.toFixed(2));
+  }
+}
+
 function openNewTransactionForm() {
 	$('open_transaction_form_link').hide();
 	$('add_new_transaction').show();
 	$('email').focus();
 	$('email').select();
 }
-
-// related to the nag form
-// function displayNagForm() {
-//  signup_form = document.getElementById("nag_form_container_container");
-//  if (signup_form.style.visibility == 'visible') {
-//    signup_form.style.visibility = 'hidden';
-//  } else {
-//    signup_form.style.visibility = 'visible';
-//    document.getElementById('nag_personal_message').focus();
-//  }
-// }
-// // capture the esc key and hide the signup form
-// if (document.layers) { document.captureEvents(Event.KEYPRESS); }
-// document.onkeypress = getKey;
-// function getKey(keyStroke) {
-//  var keyCode = (document.layers) ? keyStroke.which : keyStroke.keyCode;
-//  if (keyCode == 27) {
-//    document.getElementById("nag_form_container_container").style.visibility = 'hidden';
-//  }
-// }
-
-/***** Above this line comes from Yair's design.  Should eventually all be eliminated. *****/
 
 function finishCreateTransaction(response) {
   var json = response.responseText.evalJSON();
@@ -50,22 +42,8 @@ function failCreateTransaction(response) {
   $('new_transaction_flash').update(json.messages.error);
 }
 
-function updateGlobals(obj) {
-  // Nomworth
-  if (obj.nomworth !== undefined) {
-    var nomworth = $('nomworth');
-    if (obj.nomworth >= 0) {
-      nomworth.className = "credit";
-    } else {
-      nomworth.className = "debt";
-      obj.nomworth *= -1;
-    }
-    $('nomworth').update("$" + obj.nomworth.toFixed(2));
-  }
-}
-
 var getTodaysDateForNewTransaction = function () {
-  var months = new Array("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec");
+  var months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
   return function() {
     var today = new Date();
     return months[today.getMonth()] + ' ' + today.getDate();
