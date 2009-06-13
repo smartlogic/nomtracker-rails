37fd12f3e9ce62fa17e4a31bf7183aa9f948b1f5
John Trupiano <jtrupiano@gmail.com>
2009-06-13 11:55:21 -0400

    Improve flash notice shown after activation.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,7 +38,7 @@ class UsersController < ApplicationController
     when (!params[:activation_code].blank?) && user && user.pending?
       user.activate!
       UserMailer.deliver_activation(user)
-      flash[:notice] = "Signup complete! Please sign in to continue."
+      flash[:notice] = "Signup complete! You can now start tracking your debts."
       session[:user_id] = user.id
       redirect_to root_url
     when params[:activation_code].blank?
