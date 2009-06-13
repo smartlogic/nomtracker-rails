1dabb6c7754a0eabcb671ee03e9df3f3a13e02b6
John Trupiano <jtrupiano@gmail.com>
2009-06-13 10:27:49 -0400

    Update nomworth message when adding a transaction that takes your balance from positive to negative and vice versa.

--- rails/public/javascripts/dashboard.js
+++ rails/public/javascripts/dashboard.js
@@ -1,14 +1,22 @@
+function updateNomworth(amount) {
+  var klass = amount >= 0.0 ? "credit" : "debt";
+  var msg   = amount >= 0.0 ? "You're rich!" : "You're a deadbeat!";
+  var template = new Template("Welcome to nomtracker. Your <em>nomworth</em> is <span id='nomworth' class='#{klass}'>$#{nomworth}</span>. #{msg}");
+  $('welcome_message').update(template.evaluate({nomworth: amount.abs().toFixed(2), klass: klass, msg: msg }));
+}
+
 function updateGlobals(obj) {
   // Nomworth
   if (obj.nomworth !== undefined) {
-    var nomworth = $('nomworth');
-    if (obj.nomworth >= 0) {
-      nomworth.className = "credit";
-    } else {
-      nomworth.className = "debt";
-      obj.nomworth *= -1;
-    }
-    $('nomworth').update("$" + obj.nomworth.toFixed(2));
+    updateNomworth(obj.nomworth);
+    // var nomworth = $('nomworth');
+    // if (obj.nomworth >= 0) {
+    //   nomworth.className = "credit";
+    // } else {
+    //   nomworth.className = "debt";
+    //   obj.nomworth *= -1;
+    // }
+    // $('nomworth').update("$" + obj.nomworth.toFixed(2));
   }
 }
 
