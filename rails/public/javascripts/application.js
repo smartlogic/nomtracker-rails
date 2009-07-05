864b86fe8c6dd1725e691f02b0f3855470b0eb67
John Trupiano <jtrupiano@gmail.com>
2009-07-05 17:55:01 -0400

    Apply client-side messaging to the email address management area of the accounts tab.

--- rails/public/javascripts/application.js
+++ rails/public/javascripts/application.js
@@ -1,6 +1,35 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
+var Messaging = {
+  error   : new Template("<div class='error'>#{message}</div>"),
+  warning : new Template("<div class='warning'>#{message}</div>"),
+  info    : new Template("<div class='info'>#{message}</div>"),
+  success : new Template("<div class='success'>#{message}</div>"),
+  
+  generate : function(json) {
+    var html = "";
+    if (json) {
+      if (json.error) {
+        html += Messaging.error.evaluate({message: json.error});
+      }
+      if (json.success) {
+        html += Messaging.success.evaluate({message: json.success});
+      }
+      if (json.warning) {
+        html += Messaging.warning.evaluate({message: json.warning});
+      }
+      if (json.info) {
+        html += Messaging.info.evaluate({message: json.info});
+      }
+      if (!html) {
+        html += "Why didn't I get set?";
+      }
+    }
+    return html;
+  }
+};
+
 function updateNomworth(amount) {
   var klass = amount >= 0.0 ? "credit" : "debt";
   var msg   = amount >= 0.0 ? "You're rich!" : "You're a deadbeat!";
