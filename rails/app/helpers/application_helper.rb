78e07e89b9dbcd55ab5b8312dc846d80182623e7
John Trupiano <jtrupiano@gmail.com>
2009-07-11 13:37:53 -0400

    Visually distinguish those in your balances table on the dashboard with the 'warning' icon when they have not yet created their account.

--- rails/app/helpers/application_helper.rb
+++ rails/app/helpers/application_helper.rb
@@ -1,4 +1,13 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
-  
+
+  def display_user(user)
+    ret = h(user.primary_email.address)
+    if !user.active?
+      alt = "#{user.primary_email.address} has not yet established an account"
+      ret += link_to(image_tag('warning.gif', :alt => alt), 'javascript:void(0)', 
+        :onclick => 'alert("Bring up form to allow them to send an email to this person imploring them to join Nomtracker: unimplemented");', :title => 'Send an email asking this person to join Nomtracker')
+    end
+    ret
+  end
 end
