82316ddd81510187ffc53ed1b80563368d360848
John Trupiano <jtrupiano@gmail.com>
2009-07-12 19:42:08 -0400

    Add foreign key, ensure emails aren't left dangling when a user is deleted.

--- rails/db/migrate/20090705172123_add_foreign_keys_to_transactions.rb
+++ rails/db/migrate/20090705172123_add_foreign_keys_to_transactions.rb
@@ -2,6 +2,7 @@ class AddForeignKeysToTransactions < ActiveRecord::Migration
   def self.up
     add_foreign_key :transactions, :creditor_id, :users, :id
     add_foreign_key :transactions, :debtor_id, :users, :id
+    add_foreign_key :emails, :user_id, :users, :id
     add_index :transactions, :creditor_id
     add_index :transactions, :debtor_id
     
