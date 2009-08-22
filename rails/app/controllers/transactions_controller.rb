759091ea165949a9472cf8dd6c5cafe27d2e8bf9
John Trupiano <jtrupiano@gmail.com>
2009-08-22 10:43:21 -0400

    Rebase to master and manually merge in changes.

--- rails/app/controllers/transactions_controller.rb
+++ rails/app/controllers/transactions_controller.rb
@@ -21,6 +21,7 @@ class TransactionsController < ApplicationController
     end
 
     if @transaction.save
+      TransactionNotifier.notify_if_necessary(@transaction, current_user)
       respond_to do |format|
         format.any(:html, :json) {
           update = {
