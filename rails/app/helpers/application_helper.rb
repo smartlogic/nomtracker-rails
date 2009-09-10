bcf1471437ce41c641450bf406e7b0206a3d5ed1
Michael Berkowitz <michael.berkowitz@gmail.com>
2009-09-10 16:02:22 -0400

    switches rails image handling from file_column to paperclip.

--- rails/app/helpers/application_helper.rb
+++ rails/app/helpers/application_helper.rb
@@ -5,7 +5,7 @@ module ApplicationHelper
     ret = h(user.primary_email.address)
     if !user.active?
       alt = "#{user.primary_email.address} has not yet established an account"
-      ret += link_to(image_tag('warning.gif', :alt => alt), 'javascript:void(0)', 
+      ret += link_to(image_tag('warning.gif', :alt => alt), 'javascript:void(0)',
         :onclick => "sendInvitation(#{user.primary_email.id}, '#{escape_javascript(user.primary_email.address)}');", :title => "Send a Nomtracker invitation to #{h user.primary_email.address}")
     end
     ret
