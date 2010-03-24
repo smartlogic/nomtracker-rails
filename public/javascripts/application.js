2cf49ee097b5defba66857fc5c35819ae5728e14
Ed Schmalzle <Jonas714@gmail.com>
2010-03-24 15:46:24 -0400

    added basics of status area for page header

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -30,8 +30,8 @@ var Messaging = {
 function updateNomworth(amount) {
   var klass = amount >= 0.0 ? "credit" : "debt";
   var msg   = amount >= 0.0 ? "You're rich!" : "You're a deadbeat!";
-  var template = new Template("Welcome to nomtracker. Your <em>nomworth</em> is <span id='nomworth' class='#{klass}'>$#{nomworth}</span>. #{msg}");
-  $('welcome_message').update(template.evaluate({nomworth: amount.abs().toFixed(2), klass: klass, msg: msg }));
+  var template = new Template("Your nomworth is: <br/> <span>$#{nomworth}</span> <br/> #{msg}");
+  $('nomworth').update(template.evaluate({nomworth: amount.abs().toFixed(2), klass: klass, msg: msg }));
 }
 
 function updateGlobals(obj) {
