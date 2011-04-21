4dccac28566084802fc63bac16ef06dd65b9cdf7
Tom von Schwerdtner <tom@smartlogicsolutions.com>
2011-04-21 14:23:36 -0400

    is no find_by_login, not that this makes the feature pass, but it fails for better reasons...

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -1,5 +1,5 @@
 Given /^I login with credentials "([^\"]*)" and "([^\"]*)"$/ do |login, password|
-  @user = User.find_by_login(login)
+  @user = User.find_by_email(login)
   fill_in "login", :with => login
   fill_in "password", :with => password
   click_button "Log in"
