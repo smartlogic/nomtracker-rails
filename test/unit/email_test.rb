f25ea6bf7347d7fcfbcd0c73250e8910834ef215
Tom von Schwerdtner <tom@smartlogicsolutions.com>
2011-04-21 14:43:59 -0400

    updating to latest shoulda code, with one missing exception...

--- test/unit/email_test.rb
+++ test/unit/email_test.rb
@@ -2,14 +2,17 @@ require File.dirname(__FILE__) + '/../test_helper'
 
 class EmailTest < ActiveSupport::TestCase
   
-  should_belong_to :user
-  should_validate_presence_of :address, :user_id
-  should_validate_uniqueness_of :address
-  should_ensure_length_in_range :address, 6..100
+  should belong_to :user
+  should validate_presence_of :address
+  should validate_presence_of :address
+  should validate_uniqueness_of :address
+  # XXX: heroku
+  #should ensure_length_in_range :address, 6..100
 
-  should_have_index :user_id
-  should_have_index :address, :unique => true
-  
+  should have_db_index :user_id
+  should have_db_index :address
+  should validate_uniqueness_of :address
+
   context "When an email is instantiated without any options" do
     setup do
       @email = Email.new
