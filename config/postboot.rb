b69ca2528b76d12347172b7dd0a2a2289e3fed51
Tom von Schwerdtner <tom@smartlogicsolutions.com>
2011-04-21 11:22:56 -0400

    moving from split up env layout to standard layout, primarily becuase it makes dealing with updating gems (and using cucumber) simpler

--- config/postboot.rb
+++ config/postboot.rb
@@ -1,27 +1,27 @@
-# Be sure to restart your server when you modify this file.
-
-rails_env = ENV['RAILS_ENV'] || 'development'
-
-env_dir  = File.join(RAILS_ROOT, 'config', rails_env)
-db_file  = File.join(env_dir, 'database.yml')
-env_file = File.join(env_dir, 'environment.rb')
-
-raise "#{env_dir} environment directory cannot be found." unless File.exists?(env_dir)
-raise "#{db_file} is missing.  You cannot continue without this." unless File.exists?(db_file)
-raise "#{env_file} environment file is missing." unless File.exists?(env_file)
-
-# Now, let's open up Rails and tell it to find our environment files elsewhere.
-module Rails
-  class Configuration
-    
-    def database_configuration_file
-      File.join(root_path, 'config', environment, 'database.yml')
-    end
-    
-    # The path to the current environment's file (development.rb, etc.). By
-    # default the file is at <tt>config/environments/#{environment}.rb</tt>.
-    def environment_path
-      "#{root_path}/config/#{environment}/environment.rb"
-    end
-  end
-end
+## Be sure to restart your server when you modify this file.
+#
+#rails_env = ENV['RAILS_ENV'] || 'development'
+#
+#env_dir  = File.join(RAILS_ROOT, 'config', rails_env)
+#db_file  = File.join(env_dir, 'database.yml')
+#env_file = File.join(env_dir, 'environment.rb')
+#
+#raise "#{env_dir} environment directory cannot be found." unless File.exists?(env_dir)
+#raise "#{db_file} is missing.  You cannot continue without this." unless File.exists?(db_file)
+#raise "#{env_file} environment file is missing." unless File.exists?(env_file)
+#
+## Now, let's open up Rails and tell it to find our environment files elsewhere.
+#module Rails
+#  class Configuration
+#    
+#    def database_configuration_file
+#      File.join(root_path, 'config', environment, 'database.yml')
+#    end
+#    
+#    # The path to the current environment's file (development.rb, etc.). By
+#    # default the file is at <tt>config/environments/#{environment}.rb</tt>.
+#    def environment_path
+#      "#{root_path}/config/#{environment}/environment.rb"
+#    end
+#  end
+#end
