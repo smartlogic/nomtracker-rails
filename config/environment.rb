d166d28e094ddb18dadc447ec4d48d4594a05192
Tom von Schwerdtner <tom@smartlogicsolutions.com>
2011-04-26 15:18:55 -0400

    Try moving the require in case heroku does some magic...

--- config/environment.rb
+++ config/environment.rb
@@ -1,12 +1,12 @@
 # Be sure to restart your server when you modify this file
 
-require 'rails_sql_views'
-
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.11' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
+
+require 'rails_sql_views'
 
 Rails::Initializer.run do |config|
   # Settings in config/environments/* take precedence over those specified here.
