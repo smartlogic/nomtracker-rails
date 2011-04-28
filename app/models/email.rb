e74c2581d92d47e029594ef8e2f56ab6d579e926
Tom von Schwerdtner <tom@smartlogicsolutions.com>
2011-04-28 11:21:07 -0400

    whitespace/formatting

--- app/models/email.rb
+++ app/models/email.rb
@@ -2,24 +2,25 @@ require 'state_machine'
 
 class Email < ActiveRecord::Base
   include Authentication
-  
+
   belongs_to :user
-  
+
   validates_presence_of     :user_id, :address
-  validates_length_of       :address,    :within => 6..100 #r@a.wk
+  validates_length_of       :address, :within => 6..100 #r@a.wk
   validates_uniqueness_of   :address
-  validates_format_of       :address,    :with => Authentication.email_regex, :message => Authentication.bad_email_message
-  
+  validates_format_of       :address, :with => Authentication.email_regex,
+                            :message => Authentication.bad_email_message
+
   before_save :downcase_email!
 
   named_scope :active, :conditions => {:email_state => 'active'}
-  
+
   state_machine :email_state, :initial => :pending do
-    
+
     event :activate do
       transition :pending => :active
     end
-    
+
     state :pending do
       before_save :make_activation_code
       def make_activation_code
@@ -27,12 +28,12 @@ class Email < ActiveRecord::Base
       end
       private :make_activation_code
     end
-    
+
     state :active do
-      
+
     end
   end
-  
+
   private
     def downcase_email!
       self.address.downcase!
