0821f386af8f0f04f985e209ce97693f49c74e0b
Tom von Schwerdtner <tom@smartlogicsolutions.com>
2011-04-28 11:53:49 -0400

    User needs email assigned upon creation to be valid...

--- test/factories.rb
+++ test/factories.rb
@@ -1,10 +1,11 @@
 Factory.define :user do |u|
-  u.name 'John Doe'
+  u.name 'johndoe'
   u.password 'password'
   u.password_confirmation { |u| u.password }
+  u.email 'johndoe@example.com'
 end
 
 Factory.define :email do |e|
   e.association :user
-  e.address 'johndoe@example.com'
+  e.address 'otherguy@example.com'
 end
