3f5ec05896309207df372559918f3da90fe582d3
Tom von Schwerdtner <tom@smartlogicsolutions.com>
2011-04-28 11:29:40 -0400

    proper factory

--- test/factories.rb
+++ test/factories.rb
@@ -1,10 +1,10 @@
 Factory.define :user do |u|
   u.name 'John Doe'
   u.password 'password'
-  u.password_confirmation 'password'
+  u.password_confirmation { |u| u.password }
 end
 
 Factory.define :email do |e|
-  e.association :user, :factory => :user
+  e.association :user
   e.address 'johndoe@example.com'
 end
